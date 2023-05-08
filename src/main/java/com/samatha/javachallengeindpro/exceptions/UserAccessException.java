94dba09e76709a198efbc73c31a79e69701112b3
Vinay Neelapu <vinay.neelapu@macys.com>
2023-05-08 16:30:29 +0530

    added exception class

---
+++ src/main/java/com/samatha/javachallengeindpro/exceptions/UserAccessException.java
@@ -0,0 +1,8 @@
+
+package com.samatha.javachallengeindpro.exceptions;
+
+public class UserAccessException extends RuntimeException {
+    public UserAccessException(String message) {
+        super(message);
+    }
+}
